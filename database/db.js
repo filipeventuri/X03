ca4dfdb09127dd5294a2a5513ea2e80bae2d7f70
Filipe Venturi <filipeventuri@icloud.com>
2024-08-31 18:03:45 -0300

    Deleting database

--- database/db.js
+++
@@ -1,7 +0,0 @@
-const sequelize = require("sequelize");
-const connection = new sequelize("texts", "root", "", {
-    host:"localhost",
-    dialect:"mysql"
-});
-
-module.exports = connection;
--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 const express = require("express");
-require("dotenv").config()
 const bodyParser = require('body-parser');
-const connection = require("./database/db.js");
-const textController = require("./text/textController.js");
-const texts = require("./text/text.js");
 
 const app = express();
 
@@ -11,24 +7,10 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use("/", textController);
 
 app.get("/", (req,res)=>{
     res.render("index");  
 });
-
-app.get("/textForm",(req,res)=>{
-    res.render("form");
-});
-
-app.get("/print", (req,res)=>{
-    texts.findAll().then(text=>{
-        res.render("lista", {text:text} );
-    })
-    
-});
-
-connection.sync();
 
 const PORT = process.env.PORT || 3000;
 
